6663c97e7d4666c3c171936d5d82a6f80acc7eea
scarfacedotcom <peteradaaku@gmail.com>
2022-09-14 06:22:11 +0100

    styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,30 @@
 import PropTypes from 'prop-types'
 
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  }
 
-function Header({ text }) {
   return (
-    <header>
+    <header style={ headerStyles }>
       <div className="contaner">
         <h2>{text}</h2>
-      </div>
-      
+      </div> 
     </header>
   )
 }
 
 Header.defaultProps = {
-  text: 'Feedback UI'
+  text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95'
 }
 
 Header.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 }
 
 export default Header
