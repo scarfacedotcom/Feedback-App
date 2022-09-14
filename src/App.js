86ed14671ef58bae42cb4226b662ac3e8eb56c91
scarfacedotcom <peteradaaku@gmail.com>
2022-09-14 06:57:35 +0100

    data feedback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import FeedbackItem from './components/FeedbackItem';
 import Header from './components/Header';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        Feedback App
+        <FeedbackItem />
       </div>
     </>
   );
