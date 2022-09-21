c3c9c78f6f8fb52d003762b7584a4ed246b5d184
scarfacedotcom <peteradaaku@gmail.com>
2022-09-21 05:26:16 +0100

    fecth data

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,30 +1,26 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid'
 
 
 const FeedbackConext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This Feedback item 1',
-      rating: 10
-    },
-    {
-      id: 2,
-      text: 'This Feedback item 2',
-      rating: 9
-    },{
-      id: 3,
-      text: 'This Feedback item 3',
-      rating: 8
-    }
-  ])
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  const fetchFeedback = async () => {
+    const response = await fetch (`http://localhost:5000/feedback?_sort=id&_order=desc`)
+    const data = await response.json()
+    
+    setFeedback(data)
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Opps! Are you sure that you want to delete?')) {
